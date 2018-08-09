3b39dad35d82bc575e4ebbb35ea4680006efb9b5
PC02 <ismainressel@gmail.com>
2018-08-09 14:19:47 -0500

    Aplicación casi Terminada

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import {enableProdMode} from '@angular/core';
+
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+// Enable production mode unless running locally
+if (!/localhost/.test(document.location.host)) { enableProdMode(); }
